5f3460b2f18e122746066f3a6488d8f12225b6ce
Yu <lkmcfj@outlook.com>
2022-03-20 17:13:35 +0800

    remove minimax solver

--- solver.hpp
+++ solver.hpp
@@ -45,54 +45,6 @@ static std::vector<word_id> select_query_words(const std::vector<word_id> &words
     return ret;
 }
 
-namespace Minimax {
-    using value_t = int;
-    static value_t search(ResponseNode *node, value_t parent_best, const std::vector<word_id> &words);
-    static value_t search(GuessNode *node, const std::vector<word_id> &words) {
-        const value_t INF = 1000000000; // 1e9
-        assert(!node->child);
-        value_t min_child_value = INF;
-        int process = 0;
-        for (auto &s : select_query_words(words)) {
-            std::unique_ptr<ResponseNode> new_child = std::make_unique<ResponseNode>(node->depth + 1, s);
-            value_t cur = search(new_child.get(), min_child_value, words);
-            if (cur < min_child_value) {
-                node->child = std::move(new_child);
-                node->best_query = s;
-                min_child_value = cur;
-            }
-            if (node->depth == 0) {
-                ++process;
-                std::cerr << "root children: " << process << ", word: " << query_words[s] << ", value: " << cur << '\n';
-            }
-        }
-        return min_child_value;
-    }
-    static value_t search(ResponseNode *node, value_t parent_best, const std::vector<word_id> &words) {
-        std::vector<word_id> possible_responses[RESPONSE_RANGE];
-        for (word_id i : words) {
-            possible_responses[response_table[node->query_word][i]].push_back(i);
-        }
-        value_t max_child_value = 0;
-        if (possible_responses[0].size()) {
-            std::unique_ptr<GuessNode> cur_child = std::make_unique<GuessNode>(node->depth);
-            node->children.emplace_back(0, std::move(cur_child));
-            max_child_value = std::max(max_child_value, static_cast<value_t>(node->depth));
-        }
-        if (max_child_value >= parent_best) return max_child_value;
-        for (response_t i = 1; i < RESPONSE_RANGE; ++i) {
-            if (possible_responses[i].size()) {
-                std::unique_ptr<GuessNode> cur_child = std::make_unique<GuessNode>(node->depth);
-                value_t cur = search(cur_child.get(), possible_responses[i]);
-                max_child_value = std::max(max_child_value, cur);
-                node->children.emplace_back(i, std::move(cur_child));
-                if (max_child_value >= parent_best) return max_child_value;
-            }
-        }
-        return max_child_value;
-    }
-}
-
 namespace AverageOptimal {
     using value_t = long long;
     static value_t search(ResponseNode *node, value_t parent_best, const std::vector<word_id> &words);
